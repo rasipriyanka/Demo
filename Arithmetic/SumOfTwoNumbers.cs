1a418d7ce1bd05e8fd88cd012cf5180161f19356
priyanka <priyankapendela111@gmail.com>
2022-01-10 17:13:44 +0530

    [Rasi]:Added hello world msg implemented features in demo

---
+++ Arithmetic/SumOfTwoNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace additiontwonumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("ENTER THE VALUE OF A :- ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ENTER THE VALUE OF B :- ");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("ADDITION OF TWO NUMBER IS :- "+c);
+            Console.ReadLine();
+        }
+    }
+}
