a39a03132dea10ead16d0fabdb31b7d1dce978ab
priyanka <priyankapendela111@gmail.com>
2022-01-10 20:02:21 +0530

    Oops Concepts

---
+++ OopsConcepts/ClassAndObjectConcepts/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts.ClassAndObjectConcepts
+{
+    public class Dog
+    {
+        
+      public  string breed;
+       public  string color;
+        public static void Sleep()
+        {
+            Console.WriteLine("sleeping");     
+        }
+
+        public static void Bark()
+        {
+            Console.WriteLine("barking");
+        }
+    }
+}
---
+++ OopsConcepts/Program.cs
@@ -0,0 +1,27 @@
+﻿using OopsConcepts.ClassAndObjectConcepts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+   class Program
+    {
+      public static void Main(string[] args)
+        {
+            Dog dog1 = new Dog();
+            dog1.breed="shitzu";
+            dog1.color="white";
+            Console.WriteLine("Dog1 details"+dog1.breed+","+dog1.color);
+            Dog dog2 = new Dog();
+            dog2.breed="goldenretriver";
+            dog2.color="brown";
+            Console.WriteLine("Dog2 details"+dog2.breed+","+dog2.color);
+
+            Dog.Bark();
+            Dog.Sleep();
+        }
+    }
+}
